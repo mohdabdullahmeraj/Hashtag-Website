3a6b04f937bcc65a077299a6c399487822801578
mohdabdullahmeraj <mohdabdullahmeraj1705@gmail.com>
2025-07-05 12:04:26 +0530

    Added picture gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,13 +4,16 @@ import "./Gallery.css";
 const Gallery = () => {
   return (
     <section className="photo-gallery">
-      <div className="gallery-grid">
-        <div className="item item1"></div>
-        <div className="item item2"></div>
-        <div className="item item3"></div>
-        <div className="item item4"></div>
-        <div className="item item5"></div>
-        <div className="item item6"></div>
+      <div class="photo-gallery">
+        <div class="gallery-grid">
+          <div class="item item1"></div>
+          <div class="item item2"></div>
+          <div class="item item3"></div>
+          <div class="item item4"></div>
+          <div class="item item5"></div>
+          <div class="item item6"></div>
+          <div class="item item7"></div>
+        </div>
       </div>
     </section>
   );
